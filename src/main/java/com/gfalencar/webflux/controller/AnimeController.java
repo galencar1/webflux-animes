c6fafcc7a646e3f8bec6740ed41492809b048488
Gabriel Alencar <gfalencar02@gmail.com>
2023-01-03 15:58:44 -0300

    Implements Put method

--- src/main/java/com/gfalencar/webflux/controller/AnimeController.java
+++ src/main/java/com/gfalencar/webflux/controller/AnimeController.java
@@ -1,13 +1,14 @@
 package com.gfalencar.webflux.controller;
 
 import com.gfalencar.webflux.domain.Anime;
-import com.gfalencar.webflux.repository.AnimeRepository;
 import com.gfalencar.webflux.service.AnimeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RestController
@@ -20,13 +21,18 @@ public class AnimeController {
         return animeService.findAll();
     }
 
-    @RequestMapping(value = "getAnime/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/getAnime/{id}", method = RequestMethod.GET)
     public Mono<Anime> listById(@PathVariable String id){
         return animeService.findById(id);
     }
 
-    @RequestMapping(value = "saveAnime", method = RequestMethod.POST)
-    public Mono<Anime> saveAnime(@RequestBody Anime anime) {
+    @RequestMapping(value = "/saveAnime", method = RequestMethod.POST)
+    public Mono<Anime> saveAnime(@Valid @RequestBody Anime anime) {
         return animeService.save(anime).log();
+    }
+
+    @RequestMapping(value = "/updateAnime", method = RequestMethod.PUT)
+    public Mono<Void> updateAnime(@Valid @RequestBody Anime anime){
+        return animeService.update(anime);
     }
 }
