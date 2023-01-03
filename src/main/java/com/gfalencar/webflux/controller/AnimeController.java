cc623649dc27d4aa3bbf656cd6ee5e8286f79b25
Gabriel Alencar <gfalencar02@gmail.com>
2023-01-03 12:04:31 -0300

    Implementando FindById e Save

--- src/main/java/com/gfalencar/webflux/controller/AnimeController.java
+++ src/main/java/com/gfalencar/webflux/controller/AnimeController.java
@@ -2,23 +2,35 @@ package com.gfalencar.webflux.controller;
 
 import com.gfalencar.webflux.domain.Anime;
 import com.gfalencar.webflux.repository.AnimeRepository;
+import com.gfalencar.webflux.service.AnimeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RequiredArgsConstructor
 @RestController
 @Slf4j
 public class AnimeController {
-    private final AnimeRepository animeRepository;
+    private final AnimeService animeService;
 
     @RequestMapping(value = "/getAllAnimes", method = RequestMethod.GET)
     public Flux<Anime> listAll(){
         log.info("Buscando animes no DB!");
-        return animeRepository.findAll();
+        return animeService.findAll();
+    }
+
+    @RequestMapping(value = "getAnime/{id}", method = RequestMethod.GET)
+    public Mono<Anime> listById(@PathVariable String id){
+        log.info("Buscando anime por id");
+        log.info("Anime encontrado com o id: " + id);
+        return animeService.findById(id);
+    }
+
+    @RequestMapping(value = "saveAnime", method = RequestMethod.POST)
+    public Mono<Anime> saveAnime(@RequestBody Anime anime) {
+        log.info("Salvando Anime no DB!");
+        return animeService.save(anime).log();
     }
 }
--- src/main/java/com/gfalencar/webflux/repository/AnimeRepository.java
+++ src/main/java/com/gfalencar/webflux/repository/AnimeRepository.java
@@ -2,6 +2,12 @@ package com.gfalencar.webflux.repository;
 
 import com.gfalencar.webflux.domain.Anime;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
 
 public interface AnimeRepository extends ReactiveCrudRepository<Anime, String> {
+    @Override
+    Mono<Anime> findById(String id);
+
+    @Override
+    Mono<Anime> save(Anime anime);
 }
