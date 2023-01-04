5cc9626478c0f342f046ba2b6be72c34023a934b
Gabriel Alencar <gfalencar02@gmail.com>
2023-01-04 17:05:41 -0300

    Implements unit tests controller findById Succesful

--- src/test/java/com/gfalencar/webflux/controller/AnimeControllerTest.java
+++ src/test/java/com/gfalencar/webflux/controller/AnimeControllerTest.java
@@ -7,11 +7,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -30,15 +32,29 @@ class AnimeControllerTest {
     public void SetUp() {
         BDDMockito.when(animeServiceMock.findAll())
                 .thenReturn(Flux.just(anime));
+
+        BDDMockito.when(animeServiceMock.findById(ArgumentMatchers.anyString()))
+                .thenReturn(Mono.just(anime));
     }
 
     @Test
-    @DisplayName("")
+    @DisplayName("findAll returns a flux of anime")
     public void findAll_ReturnFluxOfAnime_WhenSuccesful(){
         StepVerifier.create(animeController.listAll())
                 .expectSubscription()
                 .expectNext(anime)
                 .verifyComplete();
     }
+
+    @Test
+    @DisplayName("findById returns a Mono With anime when it exists")
+    public void findById_ReturnMonoAnime_WhenSuccesful(){
+        StepVerifier.create(animeController.listById("63b47c98ff353c5a39c80b6a"))
+                .expectSubscription()
+                .expectNext(anime)
+                .verifyComplete();
+    }
+
+
 
 }
