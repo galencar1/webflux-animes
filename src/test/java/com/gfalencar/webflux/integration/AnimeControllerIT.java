358127652cdbe6c88f1f644a1dd8a8a372691385
Gabriel Alencar <gfalencar02@gmail.com>
2023-01-05 15:20:08 -0300

    Implements controller integration tests getAnimeById with isOk and IsNotFound

--- src/test/java/com/gfalencar/webflux/integration/AnimeControllerIT.java
+++ src/test/java/com/gfalencar/webflux/integration/AnimeControllerIT.java
@@ -1,6 +1,7 @@
 package com.gfalencar.webflux.integration;
 
 import com.gfalencar.webflux.domain.Anime;
+import com.gfalencar.webflux.exception.CustomAttributes;
 import com.gfalencar.webflux.repository.AnimeRepository;
 import com.gfalencar.webflux.service.AnimeService;
 import com.gfalencar.webflux.utils.AnimeCreator;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
@@ -16,10 +18,12 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 @ExtendWith(SpringExtension.class)
 @WebFluxTest
-@Import(AnimeService.class)
+@Import({AnimeService.class, CustomAttributes.class})
 public class AnimeControllerIT {
     @MockBean
     private AnimeRepository animeRepositoryMock; // Moca o banco de dados.
@@ -33,6 +37,9 @@ public class AnimeControllerIT {
     public void setUp(){
         BDDMockito.when(animeRepositoryMock.findAll())
                 .thenReturn(Flux.just(anime));
+
+        BDDMockito.when(animeRepositoryMock.findById(ArgumentMatchers.anyString()))
+                .thenReturn(Mono.just(anime));
     }
 
 
@@ -47,5 +54,33 @@ public class AnimeControllerIT {
                 .expectBody()
                 .jsonPath("$.[0].id").isEqualTo(anime.getId())
                 .jsonPath("$.[0].name").isEqualTo(anime.getName());
+    }
+
+    @Test
+    @DisplayName("findById returns a Mono with Anime when its exists")
+    public void findById_ReturnMonoAnime_WhenSuccesful() {
+        testClient
+                .get()
+                .uri("/getAnime/{id}","63b47c98ff353c5a39c80b6a")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Anime.class)
+                .isEqualTo(anime);
+    }
+
+    @Test
+    @DisplayName("findById returns Mono error when anime does not exists")
+    public void findById_ReturnMono_WhenEmptyMonoIsReturned(){
+        BDDMockito.when(animeRepositoryMock.findById(ArgumentMatchers.anyString()))
+                .thenReturn(Mono.empty());
+
+        testClient
+                .get()
+                .uri("/getAnime/{id}", "63b47c98ff353c5a39c80b6a")
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectBody()
+                .jsonPath("$.status").isEqualTo(404)
+                .jsonPath("$.developerMessage").isEqualTo("A ResponseStatusException Happened");
     }
 }
